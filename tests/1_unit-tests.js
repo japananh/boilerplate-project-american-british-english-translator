79dc407aa5bcf178e1da715ffb31ad84d77d058e
Tran Nhat Anh <japananh@gmail.com>
2021-03-11 23:52:26 +0700

    init commit

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,108 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
-suite('Unit Tests', () => {
+suite("Unit Tests", () => {
+  suite("Translate to British English", () => {
+    test("Translate `Mangoes are my favorite fruit.` to British English", (done) => {
+      done();
+    });
 
+    test("Translate `I ate yogurt for breakfast.` to British English", (done) => {
+      done();
+    });
+
+    test("Translate `We had a party at my friend's condo.` to British English", (done) => {
+      done();
+    });
+
+    test("Translate `Can you toss this in the trashcan for me?` to British English", (done) => {
+      done();
+    });
+
+    test("Translate `The parking lot was full.` to British English", (done) => {
+      done();
+    });
+
+    test("Translate `Like a high tech Rube Goldberg machine.` to British English", (done) => {
+      done();
+    });
+
+    test("Translate `To play hooky means to skip class or work.` to British English", (done) => {
+      done();
+    });
+
+    test("Translate `No Mr. Bond, I expect you to die.` to British English", (done) => {
+      done();
+    });
+
+    test("Translate `Dr. Grosh will see you now.` to British English", (done) => {
+      done();
+    });
+
+    test("Translate `Lunch is at 12:15 today.` to British English", (done) => {
+      done();
+    });
+  });
+
+  suite("Translate to American English", () => {
+    test("Translate `We watched the footie match for a while.` to American English", (done) => {
+      done();
+    });
+
+    test("Translate `Paracetamol takes up to an hour to work.` to American English", (done) => {
+      done();
+    });
+
+    test("Translate `First, caramelise the onions.` to American English", (done) => {
+      done();
+    });
+
+    test("Translate `I spent the bank holiday at the funfair.` to American English", (done) => {
+      done();
+    });
+
+    test("Translate `I had a bicky then went to the chippy.` to American English", (done) => {
+      done();
+    });
+
+    test("Translate `I've just got bits and bobs in my bum bag.` to American English", (done) => {
+      done();
+    });
+
+    test("Translate `The car boot sale at Boxted Airfield was called off.` to American English", (done) => {
+      done();
+    });
+
+    test("Translate `Have you met Mrs Kalyani?` to American English", (done) => {
+      done();
+    });
+
+    test("Translate `Prof Joyner of King's College, London.` to American English", (done) => {
+      done();
+    });
+
+    test("Translate `Tea time is usually around 4 or 4.30.` to American English", (done) => {
+      done();
+    });
+  });
+
+  suite("Highlight translation", () => {
+    test("Highlight translation in `Mangoes are my favorite fruit.`", (done) => {
+      done();
+    });
+
+    test("Highlight translation in `I ate yogurt for breakfast.`", (done) => {
+      done();
+    });
+
+    test("Highlight translation in `We watched the footie match for a while.`", (done) => {
+      done();
+    });
+
+    test("Highlight translation in `Paracetamol takes up to an hour to work.`", (done) => {
+      done();
+    });
+  });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,12 +1,126 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server.js');
+const server = require("../server.js");
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
+let Translator = require("../components/translator.js");
 
-suite('Functional Tests', () => {
+suite("Functional Tests", () => {
+  suite("POST /api/translate", () => {
+    test("Translation with text and locale fields", (done) => {
+      const reqBody = {};
 
+      chai
+        .request(server)
+        .post("/api/translate")
+        .send(reqBody)
+        .end((_err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(
+            res.body,
+            "solution",
+            "response should have solution"
+          );
+          done();
+        });
+    });
+
+    test("Translation with text and invalid locale field", (done) => {
+      const reqBody = {};
+
+      chai
+        .request(server)
+        .post("/api/translate")
+        .send(reqBody)
+        .end((_err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(
+            res.body,
+            "solution",
+            "response should have solution"
+          );
+          done();
+        });
+    });
+
+    test("Translation with missing text field", (done) => {
+      const reqBody = {};
+
+      chai
+        .request(server)
+        .post("/api/translate")
+        .send(reqBody)
+        .end((_err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(
+            res.body,
+            "solution",
+            "response should have solution"
+          );
+          done();
+        });
+    });
+
+    test("Translation with missing locale field", (done) => {
+      const reqBody = {};
+
+      chai
+        .request(server)
+        .post("/api/translate")
+        .send(reqBody)
+        .end((_err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(
+            res.body,
+            "solution",
+            "response should have solution"
+          );
+          done();
+        });
+    });
+
+    test("Translation with empty text", (done) => {
+      const reqBody = {};
+
+      chai
+        .request(server)
+        .post("/api/translate")
+        .send(reqBody)
+        .end((_err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(
+            res.body,
+            "solution",
+            "response should have solution"
+          );
+          done();
+        });
+    });
+
+    test("Translation with text that needs no translation", (done) => {
+      const reqBody = {};
+
+      chai
+        .request(server)
+        .post("/api/translate")
+        .send(reqBody)
+        .end((_err, res) => {
+          assert.equal(res.status, 200);
+          assert.isObject(res.body, "response should be an object");
+          assert.property(
+            res.body,
+            "solution",
+            "response should have solution"
+          );
+          done();
+        });
+    });
+  });
 });
